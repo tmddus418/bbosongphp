7456520bd26ba2492859b9a73ff0d49db8fcdd28
minju park <mlnjv016@gmail.com>
2021-05-27 05:10:02 +0900

    d

--- linkhtml/nylon.php
+++ linkhtml/nylon.php
@@ -3,26 +3,12 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   		<title>How Wash to Fabric</title>
-
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
-
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="css/fontAwesome.css">
-        <link rel="stylesheet" href="css/light-box.css">
-        <link rel="stylesheet" href="css/templatemo-style.css">
-
-        <link href="https://fonts.googleapis.com/css?family=Kanit:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-
-        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+        <link rel="stylesheet" href="linkhtml/nylon.css">
     </head>
 
     <body>
-        <h1>nylon2222</h1>
         <img src="nylon.png">
+        
                       
     </body>
