1233027c1dfcf8264fe9f2c1bde945a7617181aa
kimseungyeon <tmddus418@naver.com>
2021-05-27 02:54:01 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  		<title>How Wash to Fabric</title>
+
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/fontAwesome.css">
+        <link rel="stylesheet" href="css/light-box.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+
+        <link href="https://fonts.googleapis.com/css?family=Kanit:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+
+<body style="background-color: white;">
+
+    <div id="video-container">
+        <div class="video-overlay"style="background-color: white;"></div>
+        <div class="video-content">
+            <div class="inner" style="margin-bottom: 0%">
+                <h1 style="color:black; text-align: left; font-size: 100px; margin-right:50px; margin-left: -200px; margin-top: 100px;"><em> How<br> 
+                wash to <br></em>
+                Fabric 
+                <div class="scroll-icon">
+                    <a class="scrollTo" data-scrollTo="portfolio" href="#"><img src="img/scroll-icon.png" alt="" style="display: block; margin-left: 270px; width: 1100px; height: 700px;">
+                </div>    
+            </div>
+        </div>
+    </div>
+    
+    <div class="masonry-portfolio" id="portfolio" style="background-color: white;">
+            <div class="masonry">
+                <div class="item first-item col-md-4 col-sm-6 col-xs-12" >
+                  <a href="linkhtml/denim.php" ><div class="thumb" >
+                      
+                      <div class="image">
+                          <img src="img/denim.jpg">
+                      </div>
+                  </div></a>
+                  <div class="hover-effect">
+                    <div class="hover-content">
+                        <h1>데님</h1>
+                        <p><em>Denim</em></p>
+                    </div>
+                </div>
+                </div>
+                <div class="item col-md-4 col-sm-6 col-xs-12" >
+                  <a href="linkhtml/linen.php"><div class="thumb">
+                      
+                      <div class="image">
+                          <img src="img/linen.png">
+                      </div>
+                  </div></a>
+                  <div class="hover-effect">
+                    <div class="hover-content">
+                        <h1>린넨</h1>
+                        <p><em>Linen</em></p>
+                    </div>
+                </div>
+                </div>
+                <div class="item col-md-4 col-sm-6 col-xs-12">
+                  <a href="linkhtml/nylon.php"><div class="thumb">
+                      
+                      <div class="image">
+                          <img src="img/nylon.png">
+                      </div>
+                  </div></a>
+                  <div class="hover-effect">
+                    <div class="hover-content">
+                      <h1>나일론</h1>
+                      <p><em>Nylon</em></p>
+                    </div>
+                </div>
+                </div>
+                <div class="item col-md-4 col-sm-6 col-xs-12">
+                  <a href="linkhtml/poly.php"><div class="thumb">
+                      
+                      <div class="image">
+                          <img src="img/poly.jpeg">
+                      </div>
+                  </div></a>
+                  <div class="hover-effect">
+                    <div class="hover-content">
+                        <h1>폴리에스테르</h1>
+                        <p><em>Polyester</em></p>
+                    </div>
+                </div>
+                </div>
+                <div class="item col-md-4 col-sm-6 col-xs-12">
+                  <a href="linkhtml/cotton.php"><div class="thumb">
+                      
+                      <div class="image">
+                          <img src="img/cotton.jpg">
+                      </div>
+                  </div></a>
+                  <div class="hover-effect">
+                    <div class="hover-content">
+                        <h1>면</h1>
+                        <p><em>cotton</em></p>
+                    </div>
+                </div>
+                </div>
+                <div class="item col-md-4 col-sm-6 col-xs-12">
+                  <a href="linkhtml/silk.php"><div class="thumb">
+                      <div class="image">
+                          <img src="img/silk.jpeg">
+                      </div>
+                  </div></a>
+                  <div class="hover-effect">
+                    <div class="hover-content">
+                        <h1>실크</h1>
+                        <p><em>Silk</em></p>
+                    </div>
+                </div>
+                </div>
+                <div class="item last-item col-md-8 col-sm-6 col-xs-12" >
+                  <a href="linkhtml/wool.php"><div class="thumb">
+                      
+                      <div class="image">
+                          <img src="img/knit.jpg">
+                      </div>
+                  </div></a>
+                  <div class="hover-effect">
+                    <div class="hover-content">
+                        <h1>울</h1>
+                        <p><em>Wool</em></p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <footer style="background-color: white;">
+        <div class="container-fluid">
+            <div class="col-md-12">
+                <p style="color: black; background-color: white; font-size: 15px;padding-bottom: 20px;" >Copyright &copy; 2021 | GNU CS BBOSONG </p>
+            </div>
+        </div>
+    </footer>
+
+
+   
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+    <script src="js/vendor/bootstrap.min.js"></script>
+    
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+    
+</body>
+</html>
